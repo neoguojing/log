d6a34810dd7f5b63668a93d033bd6c70e1a4d485
guojingneo <guojing_neo@163.com>
2023-05-26 22:44:45 +0800

    up

---
+++ config.go
@@ -0,0 +1,116 @@
+package log
+
+import (
+	"os"
+
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type LogFormat string
+
+const (
+	JSON    LogFormat = "json"
+	CONSOLE LogFormat = "console"
+)
+
+var (
+	encoderConfig = zapcore.EncoderConfig{
+		TimeKey:        "timestamp",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+	encoderConsole = zapcore.NewConsoleEncoder(encoderConfig)
+	encoderJson    = zapcore.NewJSONEncoder(encoderConfig)
+
+	DefaultLogFileConfig = &lumberjack.Logger{
+		Filename:   "./app.log",
+		MaxSize:    10, // megabytes
+		MaxBackups: 5,
+		MaxAge:     28, // days
+		LocalTime:  true,
+	}
+)
+
+type Option func(*LoggerConfig)
+
+func WithComstomLogConfig(fileName string, MaxSize int, MaxAge int) Option {
+	return func(l *LoggerConfig) {
+		l.fileConfig = &lumberjack.Logger{
+			Filename:   fileName,
+			MaxSize:    MaxSize, // megabytes
+			MaxBackups: 5,
+			MaxAge:     MaxAge, // days
+			LocalTime:  true,
+		}
+	}
+}
+
+type LoggerConfig struct {
+	level      LogLevel
+	fileConfig *lumberjack.Logger
+	coreLevel  zap.AtomicLevel
+	output     zapcore.WriteSyncer
+	encoder    zapcore.Encoder
+	core       zapcore.Core
+}
+
+func NewConfig() *LoggerConfig {
+	l := &LoggerConfig{level: DEBUG}
+	l.fileConfig = DefaultLogFileConfig
+	l.coreLevel = zap.NewAtomicLevel()
+	l.coreLevel.SetLevel(zap.DebugLevel)
+	l.encoder = encoderConsole
+	l.output = zapcore.Lock(os.Stdout)
+	l.core = zapcore.NewCore(l.encoder, l.output, l.coreLevel)
+
+	return l
+}
+
+func (l *LoggerConfig) Level(level LogLevel) *LoggerConfig {
+	l.level = level
+	return l
+}
+
+func (l *LoggerConfig) Rotate(opts ...Option) *LoggerConfig {
+	l.fileConfig = DefaultLogFileConfig
+	for _, opt := range opts {
+		opt(l)
+	}
+
+	l.output = zapcore.AddSync(l.fileConfig)
+	l.core = zapcore.NewCore(l.encoder, l.output, l.coreLevel)
+	return l
+}
+
+func (l *LoggerConfig) Format(format LogFormat) *LoggerConfig {
+
+	switch format {
+	case JSON:
+		l.encoder = encoderJson
+
+	case CONSOLE:
+		l.encoder = encoderConsole
+	default:
+		l.encoder = encoderConsole
+	}
+	l.core = zapcore.NewCore(l.encoder, l.output, l.coreLevel)
+	return l
+}
+
+func (l *LoggerConfig) Build() *Logger {
+	log := &Logger{level: l.level}
+	log.logger = zap.New(l.core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
+	return log
+
+}
+
--- log.go
+++ log.go
@@ -1,11 +1,8 @@
+
 package log
 
 import (
-	"os"
-
-	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 type LogLevel int
@@ -17,105 +14,45 @@ const (
 	ERROR
 )
 
-var (
-	encoderConfig = zapcore.EncoderConfig{
-		TimeKey:        "timestamp",
-		LevelKey:       "level",
-		NameKey:        "logger",
-		CallerKey:      "caller",
-		MessageKey:     "msg",
-		StacktraceKey:  "stacktrace",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.LowercaseLevelEncoder,
-		EncodeTime:     zapcore.ISO8601TimeEncoder,
-		EncodeDuration: zapcore.StringDurationEncoder,
-		EncodeCaller:   zapcore.ShortCallerEncoder,
-	}
-	encoderConsole = zapcore.NewConsoleEncoder(encoderConfig)
-	encoderJson    = zapcore.NewJSONEncoder(encoderConfig)
-)
-
-type Option func(*Logger)
-
 type Logger struct {
-	level     LogLevel
-	logger    *zap.Logger
-	output    zapcore.WriteSyncer
-	encoder   zapcore.Encoder
-	core      zapcore.Core
-	coreLevel zap.AtomicLevel
+	level  LogLevel
+	logger *zap.Logger
 }
 
 func NewLogger() *Logger {
-	l := &Logger{level: DEBUG}
-	l.coreLevel = zap.NewAtomicLevel()
-	l.coreLevel.SetLevel(zap.DebugLevel)
-	// 设置日志输出格式
-
-	// 设置日志输出
-	l.output = zapcore.Lock(os.Stdout)
-	l.core = zapcore.NewCore(encoderConsole, l.output, l.coreLevel)
-
-	// 创建 Logger
-	l.logger = zap.New(l.core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
-	return l
-}
-
-func (l *Logger) Level(level LogLevel) *Logger {
-
-	l.level = level
-	return l
-
-}
-
-func (l *Logger) Rotate(path string) *Logger {
-	ljLogger := &lumberjack.Logger{
-		Filename:   path,
-		MaxSize:    10, // megabytes
-		MaxBackups: 5,
-		MaxAge:     28, // days
-		LocalTime:  true,
-	}
-	l.output = zapcore.AddSync(ljLogger)
-	l.core = zapcore.NewCore(l.encoder, l.output, l.coreLevel)
-	l.logger = zap.New(l.core)
-	return l
-}
-
-func (l *Logger) Format(format string) *Logger {
-
-	switch format {
-	case "json":
-		l.core = zapcore.NewCore(encoderJson, l.output, zap.NewAtomicLevelAt(zap.DebugLevel))
-	case "console":
-		l.core = zapcore.NewCore(encoderConsole, l.output, zap.NewAtomicLevelAt(zap.DebugLevel))
-	default:
-		l.core = zapcore.NewCore(encoderConsole, l.output, zap.NewAtomicLevelAt(zap.DebugLevel))
-	}
-	l.logger = zap.New(l.core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
-	return l
+	config := NewConfig()
+	return config.Build()
 }
 
 func (l *Logger) Debug(msg string) {
-	if l.level <= DEBUG {
-		l.logger.Debug(msg)
-	}
+	l.log(DEBUG, msg)
 }
 
 func (l *Logger) Info(msg string) {
-	if l.level <= INFO {
-		l.logger.Info(msg)
-	}
+	l.log(INFO, msg)
 }
 
 func (l *Logger) Warning(msg string) {
-	if l.level <= WARNING {
-		l.logger.Warn(msg)
-	}
+	l.log(WARNING, msg)
 }
 
 func (l *Logger) Error(msg string) {
-	if l.level <= ERROR {
-		l.logger.Error(msg)
+	l.log(ERROR, msg)
+}
+
+func (l *Logger) log(level LogLevel, msg string) {
+	if l.level <= level {
+		switch level {
+		case DEBUG:
+			l.logger.Debug(msg)
+		case INFO:
+			l.logger.Info(msg)
+		case WARNING:
+			l.logger.Warn(msg)
+		case ERROR:
+			l.logger.Error(msg)
+		}
 	}
 }
+
+
